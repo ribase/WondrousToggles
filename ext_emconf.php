a931225876a25a637f6f4f51de54c887bde01ba7
ClaraDevServer <ClaraDevServer>
2015-11-06 14:09:11 +0000

    initial

---
+++ ext_emconf.php
@@ -0,0 +1,54 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "Wondrous.ToggleContent".
+ *
+ * Auto generated 06-11-2015 13:57
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'ToggleContent',
+	'description' => 'ToggleContent',
+	'category' => 'misc',
+	'shy' => 0,
+	'version' => '0.0.1',
+	'dependencies' => 'cms,extbase,fluid,flux,fluidcontent,vhs',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'experimental',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 1,
+	'lockType' => '',
+	'author' => 'Sebastian Thadewald',
+	'author_email' => 'sebastian@wondrous.ch',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '4.5-6.2.99',
+			'cms' => '',
+			'extbase' => '',
+			'fluid' => '',
+			'flux' => '',
+			
+			'fluidcontent' => '',
+			'vhs' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:0:{}',
+	'suggests' => array(
+	),
+);
